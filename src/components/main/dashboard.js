ce5fd37ee5b150926d51455ea9f37c7482a48a67
Sanni Abdulfatai <esanni5@gmail.com>
2022-07-17 02:21:40 +0100

    ädd all

--- src/components/main/dashboard.js
+++ src/components/main/dashboard.js
@@ -40,7 +40,7 @@ export default function Dashboard() {
                   <FcMoneyTransfer className="text-4xl mx-auto"/>
                   <span class="font-bold">Deposited:</span>
                   <br />
-                  <span>$250</span>
+                  <span>$500</span>
                 </div>
               </div>
               <div class="card hover:shadow-xl">
@@ -48,7 +48,7 @@ export default function Dashboard() {
                   <FcDebt className="text-4xl mx-auto"/>
                   <span class="font-bold">Profit:</span>
                   <br />
-                  <span>$6500</span>
+                  <span>$7500</span>
                 </div>
               </div>
               <div class="card hover:shadow-xl">
